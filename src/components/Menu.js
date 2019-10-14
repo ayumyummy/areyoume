958bd372837de5eb65e57e5bd76565bb03b18c19
Ayumi Mamiya <vivi88lilym54@gmail.com>
2019-10-14 19:41:16 +0100

    css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,20 @@
-import { Link } from "gatsby"
+
 import React from "react"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Menu = (props) => (
     <nav id="menu">
-      <ul className="items">
-      <li><Link onClick={props.onToggleMenu} to="/contact" data-text="#3 contact" >#3 contact</Link></li>
-      <li><Link onClick={props.onToggleMenu} to="/" data-text="#2 skills & tools" >#2 skills & tools</Link></li>
-      <li><Link onClick={props.onToggleMenu} to="/about" data-text="#1 about" >#1 about</Link></li>
-      </ul>
-      <div className="lang">
-      <Link onClick={props.onToggleMenu} to="/about" data-text="en" >en</Link> / <Link onClick={props.onToggleMenu} to="/about" data-text="jp" >jp</Link>
+      <div className="main-nav">
+        <ul className="items">
+          <li><AnchorLink onClick={props.onToggleMenu} to="/contact" data-text="#3 contact" >#3 contact</AnchorLink></li>
+          <li><AnchorLink onClick={props.onToggleMenu} to="/" data-text="#2 skills & tools" >#2 skills & tools</AnchorLink></li>
+          <li><AnchorLink onClick={props.onToggleMenu} to="/about" data-text="#1 about" >#1 about</AnchorLink></li>
+        </ul>
       </div>
+      
+      {/* <div className="lang">
+      <Link onClick={props.onToggleMenu} to="/about" data-text="en" >en</Link> / <Link onClick={props.onToggleMenu} to="/about" data-text="jp" >jp</Link>
+      </div> */}
     </nav>
   )
 
