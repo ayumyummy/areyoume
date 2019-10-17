26d4f0f13dc10232f021b7ca9af581cf1036e60b
Ayumi Mamiya <a.mamiya@dewynters.com>
2019-10-17 20:19:47 +0100

    content

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -6,7 +6,7 @@ import React from "react"
 const TagLine = (props) => (
          <div className="clients">
           <div className="tagline">
-             &nbsp;Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+             &nbsp;Disney - Ubisoft - Warner Bros - Abba - Winter Wonderland - Underbelly - Japan Centre - RSC - Barbican - Theatr Clywd - Stage Traffic - MYCO - IMG - Canon - Sheik Jaber Al-Ahmad Cultural Centre
            </div>
          </div>
       
