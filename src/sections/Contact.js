17b1b6e510b0e7ce93a6584624c31e1cec033a07
Ayumi <vivi88lilym54@gmail.com>
2026-08-13 00:13:34 +0900

    Rename Layout component file

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -6,16 +6,13 @@ const Contact = (props) => (
             <h2 className="section__title-main">
                 <span className="numbering">#3</span>contact
             </h2>
-            {/* <div className="contact-method">
-                <a href="mailto:toietmoi@areyoumee.com" data-text="toietmoi@areyoumee.com"
-                className="text-large-email">toietmoi@areyoumee.com</a>
-            </div> */}
+        
             <div className="block-indent title">
               
-                <p className="indent-1"><a href="mailto:vivi88lilym54@gmail.com" rel="noopener noreferrer" data-text="> toietmoi@areyoumee.com">> vivi88lilym54@gmail.com</a></p>
-                <p className="indent-1"><a href="https://www.instagram.com/cherrychutney" target="_blank" rel="noopener noreferrer" data-text="> instagram">> instagram</a></p>
-                <p className="indent-1"><a href="https:////twitter.com/ayumyummy" target="_blank" rel="noopener noreferrer" data-text="> twitter">> twitter</a></p>
-                <p className="indent-1"><a href="https://www.linkedin.com/in/ayumimamiya/" target="_blank" rel="noopener noreferrer" data-text="> linkedin">> linkedin</a></p>
+                <p className="indent-1"><a href="mailto:vivi88lilym54@gmail.com" rel="noopener noreferrer" data-text="> vivi88lilym54@gmail.com"> vivi88lilym54@gmail.com</a></p>
+                <p className="indent-1"><a href="https://www.instagram.com/cherrychutney" target="_blank" rel="noopener noreferrer" data-text="> instagram"> instagram</a></p>
+                <p className="indent-1"><a href="https:////twitter.com/ayumyummy" target="_blank" rel="noopener noreferrer" data-text="> twitter"> twitter</a></p>
+                <p className="indent-1"><a href="https://www.linkedin.com/in/ayumimamiya/" target="_blank" rel="noopener noreferrer" data-text="> linkedin"> linkedin</a></p>
           
             </div>
         </div>
