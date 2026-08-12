ff3954f997322a2df66a2a97f2fc55b7ecc59e0f
Ayumi <vivi88lilym54@gmail.com>
2026-08-12 15:32:32 +0900

    Upgrade Gatsby dependencies for Node 22

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,10 +1,16 @@
 module.exports = [{
-      plugin: require('/Applications/MAMP/htdocs/MAMP-Site/areyoume/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/images/areyoume.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"c736f308af7bfabba104c97ed0922453"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Applications/MAMP/htdocs/MAMP-Site/areyoume/node_modules/gatsby-plugin-google-tagmanager/gatsby-browser.js'),
-      options: {"plugins":[],"id":"GTM-WZF6T52","includeInDevelopment":false},
+      plugin: require('../node_modules/gatsby-plugin-google-tagmanager/gatsby-browser.js'),
+      options: {"plugins":[],"id":"GTM-WZF6T52","includeInDevelopment":false,"defaultDataLayer":null,"routeChangeEventName":"gatsby-route-change","enableWebVitalsTracking":false,"selfHostedOrigin":"https://www.googletagmanager.com","selfHostedPath":"gtm.js"},
     },{
-      plugin: require('/Applications/MAMP/htdocs/MAMP-Site/areyoume/gatsby-browser.js'),
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,9 +1,12 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Applications/MAMP/htdocs/MAMP-Site/areyoume/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Applications/MAMP/htdocs/MAMP-Site/areyoume/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Applications/MAMP/htdocs/MAMP-Site/areyoume/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+
+
+exports.head = {
+
 }
 
